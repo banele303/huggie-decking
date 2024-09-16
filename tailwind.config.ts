d56003dd3e1eb6ffd30a41567c356251a8366515
blessed <sibandablessed724@gmail.com>
2024-09-16 06:38:47 +0000

    Me bistoz again here

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
-  		padding: '2rem',
+  	padding: '2rem',
   		screens: {
   			'2xl': '1400px'
   		}
@@ -96,22 +95,7 @@ const config = {
   					'backgroundPosition': '-200% 0'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
+  			
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
@@ -119,8 +103,7 @@ const config = {
   			scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
   			shimmer: 'shimmer 8s infinite',
   			shine: 'shine 2s  linear infinite',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			
   		}
   	}
   },
