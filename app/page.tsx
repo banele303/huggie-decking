d141a84b6b132a3dc82e1d76c4e66388aaa9a0f9
blessed <sibandablessed724@gmail.com>
2024-09-16 06:34:09 +0000

    again

--- app/page.tsx
+++ app/page.tsx
@@ -361,7 +361,7 @@ You can feel secure knowing that every one of our crew members is as dedicated t
 
       <section>
 
-        <h1 className="text-xl md:text-2xl md:text-center pt-[2rem] font-medium flex items-center gap-x-2 mx-auto justify-center">
+        <h1 className="text-xl md:text-2xl md:text-center pt-[2rem] font-medium flex i gap-x-2 md:mx-auto ">
           What our Clients are saying
 
         </h1>
@@ -371,10 +371,8 @@ You can feel secure knowing that every one of our crew members is as dedicated t
 
           <div className="flex flex-col gap-y-5 pt-4">
             <h1 className="text-lg md:text-xl ">
-              &quot;We&apos;ve been working with Bird for over 2 years and
-              they&apos;ve been amazing to work with. They&apos;ve helped us
-              grow our business and we couldn&apos;t be happier with the
-              results. &quot;
+             It has Been amazing working with Huggie Decking company , they have been transparent in each and every step of the project and the 
+             way they do the work is professional and I highly recommend them for any decking project
             </h1>
             <div className="flex items-center gap-x-1">
               <IconStarFilled className="text-4xl text-yellow-500" />
@@ -385,7 +383,7 @@ You can feel secure knowing that every one of our crew members is as dedicated t
             </div>
 
             <span className="text-xl font-medium">
-              Jordan, Brisson 
+              Sandile Mthethwa
             </span>
           </div>
           <div className="flex flex-col gap-y-5 ">
@@ -401,18 +399,14 @@ You can feel secure knowing that every one of our crew members is as dedicated t
             </div>
 
             <span className="text-xl font-medium">
-              Jordan, Brisson <br />
-              CEO, Atlas Massage
+              Jordan, Brisson 
             </span>
           </div>
 
 
           <div className="flex flex-col gap-y-5 ">
             <h1 className="text-lg md:text-xl ">
-              &quot;We&apos;ve been working with Bird for over 2 years and
-              they&apos;ve been amazing to work with. They&apos;ve helped us
-              grow our business and we couldn&apos;t be happier with the
-              results. &quot;
+             If you are looking for modern decking to elavate your house look no further they are the best when it comes to decking
             </h1>
             <div className="flex items-center gap-x-1">
               <IconStarFilled className="text-4xl text-yellow-500" />
