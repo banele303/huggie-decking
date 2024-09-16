96e6d89af764c125760daf20163637cf33d86b49
blessed <sibandablessed724@gmail.com>
2024-09-16 07:45:22 +0000

    meee

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -25,52 +25,8 @@ const AboutUS = () => {
    
     >
      
-
-     <section
-      
-        className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 
-  bg-white flex items-center 
-   justify-between py-6 px-4 md:px-8 mx-6"
-      >
-        <Link href={"/"}>
-          <Image
-            src={"/logo/logo.webp"}
-            alt="Logo"
-            width={1000}
-            height={1000}
-            className="w-28"
-          />
-        </Link>
-
-       <Navbar/>
-
-        <div className="flex items-center gap-x-4">
-          <a href="tel:519400200" className="hidden lg:flex">
-            <button className="px-4 py-2 rounded-md flex items-center gap-x-3">
-              +27 74 684 5729
-            </button>
-          </a>
-
-          <Link
-            href={"/meeting"}
-            className="
-  py-3 
-  px-6
-  text-lg 
-  hover:bg-[#abcbff]
-  rounded-[6px]
-  border-2
-  border-black
-  text-white
-  bg-[#121212]
-  transition
-  duration-200
-     hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
-          >
-            Book a call
-          </Link>
-        </div>
-      </section>
+     
+     
       <Image
         src={backgroundImage}
         alt="Hero Background"
@@ -78,7 +34,7 @@ const AboutUS = () => {
         className="absolute inset-0 z-0 "
       />
 
-
+<Navbar/>
 
 
 
